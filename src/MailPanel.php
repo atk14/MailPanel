10d3d8cdb3c1cd679b17cc7a96c74968fb68afcc
Jaromir Tomek <jaromir.tomek@gmail.com>
2025-09-27 15:35:58 +0200

    MailPanel shows all sent emails

--- src/MailPanel.php
+++ src/MailPanel.php
@@ -33,40 +33,34 @@ class MailPanel implements Tracy\IBarPanel{
 
 	function getTab(){
 		if ($this->mailer && ($this->mailer->body || $this->mailer->body_html)) {
-			return "<strong>Mailer</strong>";
+			if(method_exists($this->mailer,"getSentEmails")){
+				$count = max(sizeof($this->mailer->getSentEmails()),1);
+			}else{
+				$count = 1;
+			}
+			return "<strong>Mailer</strong> $count";
 		} else {
 			return "Mailer";
 		}
 	}
 
 	function getPanel(){
+		$emails = $this->mailer && method_exists($this->mailer,"getSentEmails") ? $this->mailer->getSentEmails() : [];
+		if(!$emails && $this->mailer && ($this->mailer->body_html || $this->mailer->body)){
+			$emails[] = $this->mailer;
+		}
+
 		$out = array();
 		$out[] = '<div style="overflow: auto;">';
 		$out[] = '<div class="tracy-MailPanel">';
-		if ($this->mailer && ($this->mailer->body_html || $this->mailer->body)) {
-			$out[] = sprintf('<div class="panel-heading"><strong>%s</strong></div>', _("Headers"));
-			$out[] = '<code id="tracy_panel_mailer_body_headers"><pre class="tracy-dump">';
-			$out[] = sprintf("Subject: %s", $this->mailer->subject);
-			$out[] = sprintf("From: %s", $this->mailer->from);
-			$out[] = sprintf("To: %s", $this->mailer->to);
-			$out[] = sprintf("Cc: %s", $this->mailer->cc);
-			$out[] = sprintf("Bcc: %s", $this->mailer->bcc);
-			$out[] = sprintf("Content-Type: %s", $this->mailer->content_type);
-			$out[] = sprintf("Content-Charset: %s", $this->mailer->content_charset);
-			$out[] = "</pre></code>";
-			if ($this->mailer->body) {
-				$out[] = sprintf('<div class="panel-heading"><strong>%s</strong></div>', _("Plain text body"));
-				$out[] = '<code id="tracy_panel_mailer_body_plain">';
-				$out[] = '<pre class="tracy-dump">';
-				$out[] = htmlspecialchars($this->mailer->body);
-				$out[] = "</pre>";
-				$out[] = "</code>";
-			}
-			if ($this->mailer->body_html) {
-				$out[] = sprintf('<div class="panel-heading"><strong>%s</strong></div>', _("HTML body"));
-				$out[] = '<div class="tracy-dump">';
-				$out[] = '<iframe style="width: 100%; height: 450px;" src="data:text/html;charset=utf-8;base64,'.base64_encode($this->mailer->body_html).'"></iframe>';
-				$out[] = "</div>";
+		if ($emails) {
+			$index = 1;
+			foreach($emails as $email){
+				if(sizeof($emails)>1){
+					$out[] = "<h2>=== Sent email #$index ===</h2>";
+				}
+				$out[] = $this->_dumpEmail($email);
+				$index++;
 			}
 		} else {
 			$out[] = '<code id="tracy_panel_mailer_body_plain">';
@@ -77,6 +71,35 @@ class MailPanel implements Tracy\IBarPanel{
 		}
 		$out[] = "</div>"; # panel panel-default
 		$out[] = "</div>";
+		return join("\n",$out);
+	}
+
+	function _dumpEmail($email){
+		$out = [];
+		$out[] = sprintf('<div class="panel-heading"><strong>%s</strong></div>', _("Headers"));
+		$out[] = '<code id="tracy_panel_mailer_body_headers"><pre class="tracy-dump">';
+		$out[] = sprintf("Subject: %s", $email->subject);
+		$out[] = sprintf("From: %s", $email->from);
+		$out[] = sprintf("To: %s", $email->to);
+		$out[] = sprintf("Cc: %s", $email->cc);
+		$out[] = sprintf("Bcc: %s", $email->bcc);
+		//$out[] = sprintf("Content-Type: %s", $email->content_type);
+		//$out[] = sprintf("Content-Charset: %s", $email->content_charset);
+		$out[] = "</pre></code>";
+		if ($email->body) {
+			$out[] = sprintf('<div class="panel-heading"><strong>%s</strong></div>', _("Plain text body"));
+			$out[] = '<code id="tracy_panel_mailer_body_plain">';
+			$out[] = '<pre class="tracy-dump">';
+			$out[] = htmlspecialchars($email->body);
+			$out[] = "</pre>";
+			$out[] = "</code>";
+		}
+		if ($email->body_html) {
+			$out[] = sprintf('<div class="panel-heading"><strong>%s</strong></div>', _("HTML body"));
+			$out[] = '<div class="tracy-dump">';
+			$out[] = '<iframe style="width: 100%; height: 450px;" src="data:text/html;charset=utf-8;base64,'.base64_encode($email->body_html).'"></iframe>';
+			$out[] = "</div>";
+		}
 		return join("\n",$out);
 	}
 }
